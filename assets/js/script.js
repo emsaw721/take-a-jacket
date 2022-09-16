e8d157cb05ca91004c14cdeca3968609280dc4ac
Emily Hill <emhillmed2021@gmail.com>
2022-09-16 16:53:59 -0500

    figuring out local storage and list items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-var userText = $(".searchbox").children("textarea").val();
+var userText = document.getElementsByClassName("searchbar").text; 
+var citiesList = [localStorage.getItem(userText)];
+
 
 
 
@@ -7,32 +9,32 @@ $(document).ready(function () {
 
     $(".srchbtn").click(function () {
         console.log("clicked");
-        localStorage.setItem("cities", userText)
-        return selectedWeatherOutput(); // this will retrieve weather data from API
-        console.log(userText); 
+        localStorage.setItem("cities", userText);
+        var userText = document.getElementsByClassName("searchbar"); 
+        // return selectedWeatherOutput(); // this will retrieve weather data from API
+        console.log(userText);
     }
     );
+    return pastSearchItems(); //this will display already searched citiies 
+}); 
 
-
+function pastSearchItems() {
     var ul = document.createElement("ul");
     document.getElementById("pastsearch").appendChild(ul);
 
 
-    var citiesList = [ $(".searchbox").children("textarea").val(localStorage.getItem(userText))];
+    for (i = 0; i < citiesList.length; i++) {
 
-    citiesList.every(renderCitiesList);
-
-    function renderCitiesList(element, index, arr) {
         var li = document.createElement("li");
-
+        li.innerText = citiesList[i]; 
         ul.appendChild(li);
 
-        li.innerHTML = li.innerHTML + element;
-    }
+
+    };
+}; 
 
 
-   
-    function selectedWeatherOutput() {
+    // function selectedWeatherOutput() {
 
-    }
-}); 
+    // }
+
