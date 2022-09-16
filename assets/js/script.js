0c6b077c36a1261d38225cc5c76818fe635e0834
Emily Hill <emhillmed2021@gmail.com>
2022-09-16 16:11:01 -0500

    fixing local storage and took off bullet point from list items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,38 @@
-var userText = $(".searchbar").siblings("textarea").val(); 
+var userText = $(".searchbox").children("textarea").val();
 
 
 
 
-$(document).ready(function() {
-    console.log(userText); 
+$(document).ready(function () {
 
-$(".srchbtn").click(function() {
-    console.log("clicked"); 
-    localStorage.setItem("cities", userText)
-    return selectedWeatherOutput(); // this will retrieve weather data from API
-}
-); 
+    $(".srchbtn").click(function () {
+        console.log("clicked");
+        localStorage.setItem("cities", userText)
+        return selectedWeatherOutput(); // this will retrieve weather data from API
+        console.log(userText); 
+    }
+    );
 
+
+    var ul = document.createElement("ul");
+    document.getElementById("pastsearch").appendChild(ul);
+
+
+    var citiesList = [ $(".searchbox").children("textarea").val(localStorage.getItem(userText))];
+
+    citiesList.every(renderCitiesList);
+
+    function renderCitiesList(element, index, arr) {
+        var li = document.createElement("li");
+
+        ul.appendChild(li);
+
+        li.innerHTML = li.innerHTML + element;
+    }
+
+
+   
+    function selectedWeatherOutput() {
+
+    }
 }); 
-
-
-function selectedWeatherOutput() {
-
-}
-
