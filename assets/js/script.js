26d71136ec6536f699668de8ece285cd60607971
Emily Hill <emhillmed2021@gmail.com>
2022-09-16 19:36:33 -0500

    starting to set up api connections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,36 @@
-// var textInput = $(".searchbar").each(function() {
-//     console.log($(this).text());
-// });
+var currentCity 
+var pastCities= []; 
+
+$(".srchbtn").click(function() {
+    console.log("click"); 
+
+    currentCity = $(this).parent().find("textarea").val(); 
+
+    if (city === "") {
+        return; 
+    }; 
+
+    pastCities.push(currentCity); 
 
 
+}
+)
 
 
-$(".srchbtn").click(function () {
-    console.log("clicked");
+// $(".srchbtn").click(function () {
+//     console.log("clicked");
 
-    var textInput = $(this).parent().find("textarea").val();
-    console.log(textInput);
-    localStorage.setItem("cities", textInput);
-    // return selectedWeatherOutput(); // this will retrieve weather data from API
+//     var textInput = $(this).parent().find("textarea").val();
+//     console.log(textInput);
+//     localStorage.setItem("cities", textInput);
+//     // return selectedWeatherOutput(); // this will retrieve weather data from API
 
-    $("#pastsearch").append("<button class='citybtn w-100' type='submit'>" + textInput + "</button"); 
+//     $("#pastsearch").append("<button class='citybtn w-100' type='submit'>" + textInput + "</button"); 
   
 
-    $("#textarea").val("");
+//     $("#textarea").val("");
 
-});
+// });
 
 
 
